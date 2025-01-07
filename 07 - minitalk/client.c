bd3edacb65baf2c92b5548b2e70bc0d0fa8456d9
ggn-thomas <thgaugai@3E5.42angouleme.fr>
2025-01-07 18:18:47 +0100

    update

--- 07 - minitalk/client.c
+++ 07 - minitalk/client.c
@@ -6,14 +6,14 @@
 /*   By: thgaugai <thgaugai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/06 09:19:54 by thgaugai          #+#    #+#             */
-/*   Updated: 2025/01/07 10:33:48 by thgaugai         ###   ########.fr       */
+/*   Updated: 2025/01/07 18:14:56 by thgaugai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 #include "libft/libft.h"
 
-void	ft_send_message(pid_t pid,char *message)
+void	ft_send_message(pid_t pid, char *message)
 {
 	int	i;
 	unsigned int	j;
@@ -22,15 +22,16 @@ void	ft_send_message(pid_t pid,char *message)
 	j = 0;
 	while (message[i])
 	{
+		j = 0;
 		while (j < 8 * sizeof(char))
 		{
-			if ((message[i] >> (7 - j)) && 1)
+			if ((message[i] >> (7 - j)) & 1)
 				kill(pid, SIGUSR1);
 			else
 				kill(pid, SIGUSR2);
 			j++;
+			usleep(100);
 		}
-		j = 0;
 		i++;
 	}
 }
@@ -38,10 +39,10 @@ void	ft_send_message(pid_t pid,char *message)
 int	main(int ac, char **av)
 {
 	pid_t	pid_server;
-
+	(void)ac;
 	if (ac != 3 || !ft_strlen(av[2]))
 		return (1);
-	pid_server = (pid_t)ft_atoi(av[1]); // convertir le pid en pid_t.
+	pid_server = (pid_t)ft_atoi(av[1]);
 	ft_send_message(pid_server, av[2]);
 	while (1)
 		pause();
--- 07 - minitalk/serveur.c
+++ 07 - minitalk/serveur.c
@@ -6,24 +6,24 @@
 /*   By: thgaugai <thgaugai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/06 09:20:05 by thgaugai          #+#    #+#             */
-/*   Updated: 2025/01/07 11:32:55 by thgaugai         ###   ########.fr       */
+/*   Updated: 2025/01/07 18:07:47 by thgaugai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 #include "libft/libft.h"
-#include "ft_printf/ft_printf.h"
 
+#include <stdio.h>
 void	translate_message(int signint)
 {
 	static int	counter = 0;
 	static char	*message = NULL;
-	char	char_received = 0;
+	static char	char_received = 0;
 	char	tmp[2];
 
 	if (!message)
 		message = ft_strdup("");
-	char_received = char_received << 1;
+	char_received <<= 1;
 	if (signint == SIGUSR1)
 		char_received |= 1;
 	counter++;
@@ -34,13 +34,14 @@ void	translate_message(int signint)
 		message = ft_strjoin(message, tmp);
 		if (char_received == '\0')
 		{
-			ft_printf("%s\n", message);
+			ft_putstr_fd(message, 1);
+			ft_putchar_fd('\n', 1);
 			free(message);
 			message = NULL;
 		}
+		char_received = 0;
+		counter = 0;
 	}
-	char_received = 0;
-	counter = 0;
 }
 
 void handle_signint(int signint)
@@ -55,7 +56,9 @@ int	main(void)
 	pid_t	pid_server;
 
 	pid_server = getpid();
-	ft_printf("Server pid : %d\n", pid_server);
+	ft_putstr_fd("Server pid : ", 1);
+	ft_putnbr_fd((int)pid_server, 1);
+	ft_putchar_fd('\n', 1);
 	sa.sa_handler = handle_signint;
 	sa.sa_flags = 0;
 	sigaction(SIGUSR1, &sa, 0);
