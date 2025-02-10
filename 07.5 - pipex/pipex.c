50edc7f64f62057283b3dc5fd702e78d8a99472b
thomas <thomas.gaugain0702@gmail.com>
2025-02-10 15:56:18 +0100

    push

--- 07.5 - pipex/pipex.c
+++ 07.5 - pipex/pipex.c
@@ -1,21 +1,43 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   swap_bits.c                                        :+:      :+:    :+:   */
+/*   pipex.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: thomas <thomas@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/01/29 15:20:57 by thgaugai          #+#    #+#             */
-/*   Updated: 2025/01/30 08:41:01 by thomas           ###   ########.fr       */
+/*   Created: 2025/02/07 10:38:59 by thomas            #+#    #+#             */
+/*   Updated: 2025/02/07 14:26:08 by thomas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-unsigned char	swap_bits(unsigned char octet)
+#include "pipex.h"
+
+void    pipex(char **av)
 {
-	return (octet << 4 | octet >> 4);
+    
 }
 
-unsigned char	swap_bits(unsigned char octet)
+
+
+int main(int ac, char **av)
 {
-	return(octet << 4 | 4 >> octet);
-}
+    int fd[2];
+    int pid;
+    
+    pid = 0;
+    if (ac != 5)
+        return (1);
+    if (pipe(fd) == -1)
+        return (1);
+    pid = fork();
+    if (pid == 0)
+    {
+        close(fd[0]);
+        dup2(fd[1], STDOUT_FILENO);
+        close(fd[1]);
+        execve(commande, nom_du_fichier, NULL);
+        
+    }
+    return (0);
+} 
+
--- 07.5 - pipex/pipex.h
+++ 07.5 - pipex/pipex.h
@@ -1,36 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   reverse_bits.c                                     :+:      :+:    :+:   */
+/*   pipex.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: thomas <thomas@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/01/29 15:22:17 by thgaugai          #+#    #+#             */
-/*   Updated: 2025/01/30 08:40:24 by thomas           ###   ########.fr       */
+/*   Created: 2025/02/07 10:41:49 by thomas            #+#    #+#             */
+/*   Updated: 2025/02/07 10:42:35 by thomas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-unsigned char	reverse_bits(unsigned char octet)
-{
-	unsigned char	r = 0;
-	unsigned	byte_len = 8;
+#ifndef PIPEX_H
+#define PIPEX_H
 
-	while (byte_len--)
-	{
-		r = (r << 1) | (octet & 1);
-		octet >>= 1;
-	}
-	return (r);
-}
-unsigned char	reverse_bits(unsigned char octet)
-[
-	unsigned char	r = 0;
-	unsigned	byte_len = 8;
-	
-	while (byte_len--)
-	{
-		r = (r << 1) | (octet & 1);
-		octet<<=1;
-	}
-	return (r);
-]
+#include <fcntl.h>
+
+#endif
