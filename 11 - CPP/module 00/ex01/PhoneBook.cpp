aa09969278e98df2061099a1db5d2fbffc04d052
thomas <thomas.gaugain0702@gmail.com>
2025-05-05 13:42:40 +0200

    push

--- 11 - CPP/module 00/ex01/PhoneBook.cpp
+++ 11 - CPP/module 00/ex01/PhoneBook.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   PhoneBook.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: thgaugai <thgaugai@student.42.fr>          +#+  +:+       +#+        */
+/*   By: thomas <thomas@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/29 10:26:02 by thomas            #+#    #+#             */
-/*   Updated: 2025/05/01 18:04:59 by thgaugai         ###   ########.fr       */
+/*   Updated: 2025/05/04 14:33:11 by thomas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,30 +60,52 @@ void PhoneBook::print_string(std::string str)
 
 int	PhoneBook::handle_index(std::string index)
 {
-	if (index.isAlpha())
+	if (index.empty())
+		return (-1);
+	if (!(std::isdigit(index[0])))
 		return (-1);
 	if (index.length() > 1)
 		return (-1);
-	return (index + '0');
+	return (index[0] - '0');
 }
+
 
 void PhoneBook::display_result(std::string index)
 {
-	int	num = 1000000;
-
-	while ((!(num < count)) || handle_index(index) == -1)
+	int	index_int = 0;
+		
+	while (1)
 	{
-		std::cout << "\033[31mInvalid index ! Retry with another value : \033[0m";
-		std::cin>>index;
-		if (std::cin.eof())
-			exit(0);
+		index_int = handle_index(index);
+		if (index_int == -1)
+		{
+			std::cout << "\033[31mPlease enter valid index!\033[0m" << std::endl;
+			index.clear();
+			std::cout << "Enter the index of the contact you want : ";
+			std::getline(std::cin, index);
+			if (std::cin.eof())
+				exit(0);
+			continue;
+		}
+		if (index_int >= count || index_int < 0)
+		{
+			std::cout << "\033[31mPlease enter valid index!\033[0m" << std::endl;
+			index.clear();
+			std::cout << "Enter the index of the contact you want : ";
+			std::getline(std::cin, index);
+			if (std::cin.eof())
+				exit(0);
+			continue;
+		}
+		else
+			break;
 	}
 	std::cout << "Here is the contact number : " << index << std::endl;
-	std::cout << contact[index].getFirstname() << std::endl;
-	std::cout << contact[index].getLastname() << std::endl;
-	std::cout << contact[index].getNickname() << std::endl;
-	std::cout << contact[index].getPhonenumber() << std::endl;
-	std::cout << contact[index].getDarkestsecret() << std::endl;
+	std::cout << contact[index_int].getFirstname() << std::endl;
+	std::cout << contact[index_int].getLastname() << std::endl;
+	std::cout << contact[index_int].getNickname() << std::endl;
+	std::cout << contact[index_int].getPhonenumber() << std::endl;
+	std::cout << contact[index_int].getDarkestsecret() << std::endl;
 }
 
 void	PhoneBook::display_allcontact()
@@ -96,10 +118,10 @@ void	PhoneBook::display_allcontact()
 		std::cout << "\033[31mYou haven't got contact in your phonebook !\033[0m\n" << std::endl;
 			return ;
 	}
+	std::cout << "|     Index| Firstname|  Lastname|  Nickname|" << std::endl;
+	std::cout << "|----------|----------|----------|----------|" << std::endl;
 	while (i < count)
 	{
-		std::cout << "|     Index| Firstname|  Lastname|  Nickname|" << std::endl;
-		std::cout << "|----------|----------|----------|----------|" << std::endl;
 		std::cout << "|         " << i << '|';
 		print_string(contact[i].getFirstname());
 		std::cout << '|';
@@ -111,6 +133,6 @@ void	PhoneBook::display_allcontact()
 		i++;
 	}
 	std::cout << "Enter the index of the contact you want : ";
-	std::cin >> index;
+	std::getline(std::cin, index);
 	display_result(index);
 }
