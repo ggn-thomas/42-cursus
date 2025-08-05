390f45b64292e3c2b0b4f5161382751268fa4ace
Thomas Gaugain <thgaugai@1E2.42angouleme.fr>
2025-08-05 19:30:21 +0200

    PUSH

--- 13 - Inception/srcs/requirements/mariadb/tools/script.sh
+++ 13 - Inception/srcs/requirements/mariadb/tools/script.sh
@@ -9,7 +9,7 @@ done
 
 echo "MariaDB est prêt, configuration de la base de données..."
 
-mysql -u root << EOF
+mysql -u root -p$MYSQL_ROOT_PASSWORD << EOF
 CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\`;
 CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
 GRANT ALL PRIVILEGES ON \`$MYSQL_DATABASE\`.* TO '$MYSQL_USER'@'%';
